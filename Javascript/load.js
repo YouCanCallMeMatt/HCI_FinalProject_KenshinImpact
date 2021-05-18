e3d8c2ce44b8814728228c1b20d861c267c94389
astriddwrn <astrid.dewi.rana@gmail.com>
2021-05-19 00:47:00 +0700

    footer & navbar added

---
+++ Javascript/load.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+  $("#navbar-partial").load("./Partials/navbar-partial.html");
+  $("#footer-partial").load("./Partials/footer-partial.html");
+});
---
+++ Javascript/navbar.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $(".menu-icon").click(function () {
+    $(".menu-container").slideToggle("slow");
+  });
+});
