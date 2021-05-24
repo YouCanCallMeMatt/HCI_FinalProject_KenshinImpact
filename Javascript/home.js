3699f50ebc837d9595ac3bd2d4a3fe412bd73b3b
astriddwrn <astrid.dewi.rana@gmail.com>
2021-05-25 01:44:09 +0700

    characters section done

--- Javascript/home.js
+++ Javascript/home.js
@@ -12,18 +12,42 @@ $(document).ready(function () {
 
   $show = 0;
 
-  $("slide").eq($show).css("opacity", 1);
+  $(".hero .slide").eq($show).css("opacity", 1);
 
-  $(".arrow").click(function () {
-    $(".slide").eq($show).removeClass("active");
-    $(".dots li").eq($show).removeClass("dot-active");
+  $(".hero .arrow").click(function () {
+    $(".hero .slide").eq($show).removeClass("active");
+    $(".hero .dots li").eq($show).removeClass("dot-active");
     if ($(this).hasClass("prev")) {
       $show -= 1;
     } else {
       $show += 1;
     }
     $show = showCheck($show);
-    $(".slide").eq($show).addClass("active");
-    $(".dots li").eq($show).addClass("dot-active");
+    $(".hero .slide").eq($show).addClass("active");
+    $(".hero .dots li").eq($show).addClass("dot-active");
+  });
+
+  $mid = 0;
+  $l = 3;
+  $r = 1;
+  $(".characters .arrow").click(function () {
+    $(".characters .card").eq($l).removeClass("left");
+    $(".characters .card").eq($mid).removeClass("middle");
+    $(".characters .card").eq($r).removeClass("right");
+    if ($(this).hasClass("prev")) {
+      $mid--;
+      $l--;
+      $r--;
+    } else {
+      $mid++;
+      $l++;
+      $r++;
+    }
+    $mid = showCheck($mid);
+    $l = showCheck($l);
+    $r = showCheck($r);
+    $(".characters .card").eq($l).addClass("left");
+    $(".characters .card").eq($mid).addClass("middle");
+    $(".characters .card").eq($r).addClass("right");
   });
 });
